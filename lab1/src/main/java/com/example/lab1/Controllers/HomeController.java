38202fe618f13a3a40d10d6e071ac5bb5ebd9e4a
Dylan Bustamante <dylan.bustamante@pucp.edu.pe>
2023-08-24 21:10:19 -0500

    nadbar

--- lab1/src/main/java/com/example/lab1/Controllers/HomeController.java
+++ lab1/src/main/java/com/example/lab1/Controllers/HomeController.java
@@ -1,10 +1,16 @@
 package com.example.lab1.Controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class HomeController {
-    
+
+    @RequestMapping(value = {"/principal"})
+    public String inicio(){
+        return "paginaInicial";
+    }
 
 }
