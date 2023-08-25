5fb6391c2787a4bd7a8cf4a4bac5b863e6a58b7d
Dylan Bustamante <dylan.bustamante@pucp.edu.pe>
2023-08-24 20:45:29 -0500

    RAMA develop

---
+++ lab1/src/main/java/com/example/lab1/Controllers/HomeController.java
@@ -0,0 +1,10 @@
+package com.example.lab1.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class HomeController {
+    
+
+}
